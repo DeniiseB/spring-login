fe21c2ffa61336a6f5fcc3c07125a610f127e98e
Denise <72066356+DeniiseB@users.noreply.github.com>
2021-11-24 17:14:45 +0100

    update in demoApp

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,12 +3,20 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(DemoApplication.class, args);
+	//	SpringApplication.run(DemoApplication.class, args);
+
+		String PORT = System.getenv("PORT");
+
+		SpringApplication app = new SpringApplication(DemoApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", PORT == null ? 4000 : PORT));
+		app.run(args);
 	}
 
 }
